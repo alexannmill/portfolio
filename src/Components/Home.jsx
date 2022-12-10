bd36b5f62f5ff66c51cd058304ef4235daa1d9e0
Alex Miller <alexannmill@gmail.com>
2022-12-09 17:20:28 -0800

    home page content need style

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,18 @@
 export default function Home() {
   return (
-    <div className="home">
-      <h1 className="name">ALEX MILLER</h1>
-      <h3 className="profession">Full Stack Developer</h3>
-      <p className="blurb">I used to make beer and now I make websites.</p>
-    </div>
+    <>
+      <div className="img-container">
+        <img
+          className="home-bg-img"
+          src="https://cdn.dribbble.com/users/846370/screenshots/16009081/media/00b25e745f40864f8d9c727ed5006361.jpg"
+          alt="splat"
+        ></img>
+      </div>
+      <div className="home">
+        <h1 className="hi">Hi, I'm</h1>
+        <h1 className="name"> ALEX MILLER.</h1>
+        <p className="blurb">I used to make beer and now I make websites.</p>
+      </div>
+    </>
   );
 }
