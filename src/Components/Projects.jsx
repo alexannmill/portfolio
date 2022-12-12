bd8a113600dc868959f22b76c43cd5e30b2ba6ce
Alex Miller <alexannmill@gmail.com>
2022-12-11 23:31:51 -0800

    project card first project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -4,16 +4,17 @@ import Card from "react-bootstrap/Card";
 export default function Projects() {
   return (
     <div className="projects">
-      <Card style={{ width: "18rem" }}>
+      <Card className="card" style={{ width: "18rem" }}>
         <Card.Img
           variant="top"
           src="https://media.istockphoto.com/id/1225201968/photo/hand-in-a-pop-art-collage-style-in-neon-bold-colors-modern-psychedelic-creative-element-with.jpg?b=1&s=612x612&w=0&k=20&c=HkQC8zYzUFoIc-k0YHFTja2PtOTukpJWsqW8e6tRo-c="
         />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>Safeguardian</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            Work-in-progress: A map application for unhoused people to find
+            resources. Hoping one day mall-screen kiosk are up around the city
+            giving information to those in need.
           </Card.Text>
           <Button variant="dark">GitHub</Button>
         </Card.Body>
