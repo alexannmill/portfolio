bc1eebc07eb8f49308e3c023b2ffc4f8a0b090d6
Alex Miller <alexannmill@gmail.com>
2022-12-15 15:39:22 -0800

    repositioning

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,15 +4,15 @@ import photo from "./imgs/IMG_0528.JPG";
 export default function Home() {
   return (
     <>
-      <div className="img-container">
-        <img className="home-bg-img" src={photo} alt="me"></img>
-      </div>
       <div className="home">
-        <div className="slant">
-          <h1 className="hi">Hi, I'm</h1>
-          <h1 className="name"> ALEX MILLER.</h1>
+        <div className="img-container">
+          <img className="home-bg-img" src={photo} alt="me"></img>
         </div>
-        <p className="blurb">I used to make beer and now I make websites.</p>
+        <div className="slant">
+          <h1 className="hi">Hi, my name is </h1>
+          <h1 className="name"> Alex Miller.</h1>
+          <p className="blurb">Ex-Brewer turned Software Developer</p>
+        </div>
       </div>
     </>
   );
