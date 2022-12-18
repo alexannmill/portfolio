497f16db324074ca9f0b9f766d2a362eba929b13
Alex Miller <alexannmill@gmail.com>
2022-12-17 17:19:52 -0800

    little styling touches

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -12,10 +12,10 @@ export default function NavBar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-            <Nav.Link href="#blog">Beer Blog</Nav.Link>
+            <Nav.Link href="#home">// About</Nav.Link>
+            <Nav.Link href="#projects">// Projects</Nav.Link>
+            <Nav.Link href="#contact">// Contact</Nav.Link>
+            <Nav.Link href="#blog"> // BeerBlog</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
