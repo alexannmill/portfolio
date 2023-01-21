9b66a826f6ec3f366a2159c40c1861368585583f
Alex Miller <alexannmill@gmail.com>
2023-01-21 15:36:58 -0800

    skill list added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,12 +1,79 @@
+import "./Styles/Skills.css";
+
 export default function Home() {
+  const backend = [
+    "SQL",
+    "Node.js",
+    "Javascript",
+    "Typescript",
+    "Ruby",
+    "Express",
+  ];
+  const frontend = [
+    "Javascript",
+    "Typescript",
+    "React",
+    "HTML",
+    "CSS",
+    "jQuery",
+  ];
+
+  const webDesign = ["Figma", "Canva", "Tailwaind", "Bootstrap", "ERDs"];
+  const strategies = ["Agile", "Git", "Testing", "Documentation"];
+
+  const renderFrontend = frontend.map((skill) => {
+    return (
+      <div className="skill">
+        <h3>{skill}</h3>
+      </div>
+    );
+  });
+  const renderWebDesign = webDesign.map((skill) => {
+    return (
+      <div className="skill">
+        <h3>{skill}</h3>
+      </div>
+    );
+  });
+  const renderBackend = backend.map((skill) => {
+    return (
+      <div className="skill">
+        <h3>{skill}</h3>
+      </div>
+    );
+  });
+
+  const renderStrategies = strategies.map((skill) => {
+    return (
+      <div className="skill">
+        <h3>{skill}</h3>
+      </div>
+    );
+  });
+
   return (
-    <>
-      <div className="skills"></div>
-      <p align="center">
-        <a href="https://skillicons.dev">
-          <img src="https://skillicons.dev/icons?i=js,html,css,typescript,react,nodejs,docker,jest,jquery,postgres,mysql,ruby,vscode,tailwind,express" />
-        </a>
-      </p>
-    </>
+    <div className="skills">
+      <h1 className="skill-set-title">
+        <strong>SKILL SET</strong>
+      </h1>
+      <div className="skill-list">
+        <div className="list">
+          <h2>// Font-end : </h2>
+          {renderFrontend}
+        </div>
+        <div className="list">
+          <h2>// Back-end :</h2>
+          {renderBackend}
+        </div>
+        <div className="list">
+          <h2>// Web Design :</h2>
+          {renderWebDesign}
+        </div>
+        <div className="list">
+          <h2>// Strategies : </h2>
+          {renderStrategies}
+        </div>
+      </div>
+    </div>
   );
 }
