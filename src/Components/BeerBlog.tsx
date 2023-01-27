93aa419c6021e4431ce019f3a75a5f37b6ac5a72
Alex Miller <alexannmill@gmail.com>
2023-01-27 14:02:28 -0800

    react scroll added

--- src/Components/BeerBlog.tsx
+++ src/Components/BeerBlog.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Styles/Projects.css";
-import {motion} from "framer-motion"
+import { motion } from "framer-motion"
 
 export default function BeerBlog() {
   return (
