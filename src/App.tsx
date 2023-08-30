9a7cb7fb60b4709c868f68461d90b8ab6954d5f3
alexezpt <alexannmill@gmail.com>
2023-08-29 19:12:40 -0700

    Replaced page query with react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
-/* eslint-disable jsx-a11y/anchor-has-content */
-import NavBar from "./Components/NavBar";
-import Home from "./Components/Home";
-import Contact from "./Components/Contact";
-import Projects from "./Components/Projects";
-import BeerBlog from "./Components/BeerBlog";
-import Skills from "./Components/Skills";
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import Contact from './Components/Contact';
+import Projects from './Components/Projects';
+import BeerBlog from './Components/BeerBlog';
+import Skills from './Components/Skills';
+import Footer from './Components/Footer';
+import './Components/Styles/NavBar.css';
 
 export default function App() {
-
-  return (
-    <div className="App">
-      <NavBar />
-      <a id="home"></a>
-      <Home />
-      <a id="skills"></a>
-      <Skills />
-      <a id="projects"></a>
-      <Projects />
-      <a id="blog"></a>
-      <BeerBlog />
-      <a id="contact">.</a>
-      <Contact />
-    </div>
-  );
+	return (
+		<div className='App'>
+			<NavBar />
+			<div className='outlet-container'>
+				<Router>
+					<Routes>
+						<Route path='/home' element={<Home />} />
+						<Route path='/skills' element={<Skills />} />
+						<Route path='/projects' element={<Projects />} />
+						<Route path='/beerblog' element={<BeerBlog />} />
+						<Route path='/contact' element={<Contact />} />
+					</Routes>
+				</Router>
+			</div>
+			<Footer />
+		</div>
+	);
 }
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -9,32 +9,33 @@ import { faTerminal } from '@fortawesome/free-solid-svg-icons'
 
 export default function NavBar() {
   return (
-    <Navbar className="navbar" fixed="top" expand="lg">
-      <Container>
-        <Navbar.Brand href="#home">
-          <h1 className="logo">
-            <FontAwesomeIcon icon={faTerminal} />
-            <strong>@|ex Mi||er</strong>
-          </h1>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">// About</Nav.Link>
-            <Nav.Link href="#projects">// Projects</Nav.Link>
-            <Nav.Link href="#contact">// Contact</Nav.Link>
-            <Nav.Link href="#blog">// BeerBlog</Nav.Link>
-            <Nav.Link href="https://github.com/alexannmill">// Github</Nav.Link>
-            <Nav.Link
-              href="https://resume.creddle.io/resume/idi71kas4cy"
-              target="_blank"
-            >
-              {" "}
-              // Resume
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+		<Navbar className='navbar' fixed='top' expand='lg'>
+			<Container>
+				<Navbar.Brand href='#home'>
+					<h1 className='logo'>
+						<FontAwesomeIcon icon={faTerminal} />
+						<strong>@|ex Mi||er</strong>
+					</h1>
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='me-auto'>
+						<Nav.Link href='/home'>// About</Nav.Link>
+						<Nav.Link href='/skills'>// Skills </Nav.Link>
+						<Nav.Link href='/projects'>// Projects</Nav.Link>
+						<Nav.Link href='/contact'>// Contact</Nav.Link>
+						<Nav.Link href='/beerblog'>// BeerBlog</Nav.Link>
+						<Nav.Link href='https://github.com/alexannmill'>// Github</Nav.Link>
+						<Nav.Link
+							href='https://resume.creddle.io/resume/idi71kas4cy'
+							target='_blank'
+						>
+							{' '}
+							// Resume
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
