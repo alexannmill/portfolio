25d2fd83db1503f505a781412dfc0d1d66af5b67
alexezpt <alexannmill@gmail.com>
2023-08-30 19:55:04 -0700

    .

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,7 @@ export default function App() {
 	// 		],
 	// 	},
 	// ]);
+
 	return (
 		<div className='App'>
 			<NavBar />
--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -6,32 +6,35 @@ import Navbar from "react-bootstrap/Navbar";
 import "./Styles/NavBar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTerminal } from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
 
 export default function NavBar() {
 	return (
 		<div>
 			<Navbar className='navbar' fixed='top' expand='lg'>
 				<Container>
-					<Navbar.Brand href='/'>
-						<h1 className='logo'>
-							<FontAwesomeIcon icon={faTerminal} />
-							<strong>@|ex Mi||er</strong>
-						</h1>
-					</Navbar.Brand>
+					<Link to='/portfolio/'>
+						<Navbar.Brand>
+							<h1 className='logo'>
+								<FontAwesomeIcon icon={faTerminal} />
+								<strong>@|ex Mi||er</strong>
+							</h1>
+						</Navbar.Brand>
+					</Link>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
 						<Nav className='me-auto'>
-							<Nav.Link href='/'>// About</Nav.Link>
-							<Nav.Link href='/skills'>// Skills </Nav.Link>
-							<Nav.Link href='/projects'>// Projects</Nav.Link>
-							<Nav.Link href='/contact'>// Contact</Nav.Link>
-							{/* <Nav.Link href='/beerblog'>// BeerBlog</Nav.Link> */}
-							<Nav.Link
-								href='https://docs.google.com/document/d/e/2PACX-1vRcPQM5MEqFcaweZ-DP_fYSzJ5wnkDl-6fEeZ_799kjUNDDl5vtxjbKhxrKKBSn8Qh_og_QoCu36QBs/pub'
+							<Link to='/portfolio/'>// About</Link>
+							<Link to='/portfolio/skills'>// Skills </Link>
+							<Link to='/portfolio/projects'>// Projects</Link>
+							<Link to='/portfolio/contact'>// Contact</Link>
+							{/* <Link to='/beerblog'>// BeerBlog</Link> */}
+							<Link
+								to='https://docs.google.com/document/d/e/2PACX-1vRcPQM5MEqFcaweZ-DP_fYSzJ5wnkDl-6fEeZ_799kjUNDDl5vtxjbKhxrKKBSn8Qh_og_QoCu36QBs/pub'
 								target='_blank'
 							>
 								// Resume
-							</Nav.Link>
+							</Link>
 						</Nav>
 					</Navbar.Collapse>
 				</Container>
